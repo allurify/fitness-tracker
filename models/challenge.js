e750b17c83d0350af3471ace3d9fea6945d7b822
druhi <druhi.mehra@gmail.com>
2025-07-03 11:29:55 +0530

    challenge.js

--- models/challenge.js
+++ models/challenge.js
@@ -1,1 +1,18 @@
 // challenge schema collection
+
+
+const mongoose = require('mongoose');
+
+const ChallengeSchema = new mongoose.Schema({
+  name: String,
+  desc: String,
+  goalType: String,
+  duration: Number,
+  routineIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Routine' }],
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  startDate: Date,
+  endDate: Date,
+  isActive: Boolean
+});
+
+module.exports = mongoose.model('Challenge', ChallengeSchema);
